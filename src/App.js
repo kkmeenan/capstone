c2267dd4a58266b8e123f3a40610104a3c1a4f6c
Kendall <kkmeenan@gmail.com>
2020-11-22 17:54:34 -0500

    enter button functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 function App() {
-  return null
+  const [userid, setID] = useState([])
+  const IDRef = useRef()
+
+  function handleID() {
+ 	const id = IDRef.current.value
+	if (id === '') return
+	setID(prevID => {
+		return [id]
+	})
+	IDRef.current.value = null
+  }
+
+  return (
+	<>
+		<div>
+		Stress Level Indicator	
+		</div>
+		<input ref={IDRef} type="text" />
+		<button onClick ={handleID}>Enter ID</button>
+	</>
+   )
 }
 
 export default App;
