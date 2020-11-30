ffb71c153775d492e3b3b48075d3fc2f341d8740
Elena <sir.elena.lvaose@gmail.com>
2020-11-30 14:04:55 -0500

    get fetch to work

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
   const [userid, setID] = useState([])
-  const [sdata, setSdata] = useState([]);
+  const [sdata, setSdata] = useState("");
   const IDRef = useRef()
-  const APIURL = "scares-api-dev.us-east-1.elasticbeanstalk.com/getdata?user_id="
+  const APIURL = "http://scares-api-dev.us-east-1.elasticbeanstalk.com/getdata?user_id="
 
   //get the userID typed into the text box
   function handleID() {
@@ -19,25 +19,48 @@ function App() {
   }
 
   //make call to database
-  useEffect(() => { 
-	fetch(`${APIURL}${userid}&time=${new Date().toISOString}`, {
-		headers: {
-			'Authorization' : 'Token'
-		}
-	})
-	.then(res => res.json())
-	.then(
-		(result) => {
-			setIsLoaded(true);
-			setSdata(result)
-		},
-		(error) => {
-			setIsLoaded(true);
-			setError(error);
-		}
-	)
-  }, [])
+  // useEffect(() => {
+	// fetch(`${APIURL}${userid}&time=${new Date().toISOString}`, {
+	// 	headers: {
+	// 		'Authorization' : 'Token'
+	// 	}
+	// })
+	// .then(res => {
+	// 	console.log(res)
+	// 	res.json()
+	// })
+	// .then(
+	// 	(result) => {
+	// 		setIsLoaded(true);
+	// 		setSdata(result)
+	// 	},
+	// 	(error) => {
+	// 		setIsLoaded(true);
+	// 		setError(error);
+	// 	}
+	// )
+  // }, []);
 
+  function getState() {
+  	let date = new Date().toISOString();
+
+		fetch(`${APIURL}${userid}&time=${date}`, {
+			headers: {
+				'Authorization' : 'Token'
+			}
+		})
+			.then(res => {
+					return res.json()
+				})
+			.then(res => {
+				setIsLoaded(true);
+				setSdata(res)
+				},
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				})
+	}
 
   return (
 	<>
@@ -46,8 +69,12 @@ function App() {
 		</div>
 		<input ref={IDRef} type="text" />
 		<button onClick ={handleID}>Enter ID</button>
+		<button onClick ={getState}>Load</button>
 		<li>
 		User ID Entered: {userid}
+		</li>
+		<li>
+			User's State is: {sdata.state} at time {sdata.time}
 		</li>
 	</>
    )
